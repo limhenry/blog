4fb17d2493444890f8bf55ed6be716d7e035db38
henrylim <henry71896@gmail.com>
2015-07-13 17:19:42 +0800

    Update

--- adsense.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-        setTimeout("checkAds();", 1000);
-});
-
-function checkAds() {
-        if ($(".adsense").height() == "0") {
-                $(".adsense").after("<div class='alt'><h3>Don't like ads?</h3><p>Me neither.</p><p>But you should know that the ads on this site help pay for hosting, domain renewal, and keeping me motivated to make other oddball apps and websites.</p><p>Please consider disabling your adblock software for this domain. In return, I'll give you an appreciative high-five if we ever meet in real life.</p><p>-Tommy</p></div>");
-        }
-}
